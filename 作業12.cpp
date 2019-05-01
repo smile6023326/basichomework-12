8e5d4bce12dc49d9ef16e66c520b558cb833f84f
smile6023326 <48814064+smile6023326@users.noreply.github.com>
2019-05-01 23:38:05 +0800

    Add files via upload

---
+++ 作業12.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x = 5;
+	printf("½Ð¿é¤J¼Æ¦r\n");
+	scanf("%d",&x);
+	printf("*****\n");
+	printf(" ****\n");
+	printf("  ***\n");
+	printf("   **\n");
+	printf("    *\n");
+	system("pause");
+	return 0;
+}
